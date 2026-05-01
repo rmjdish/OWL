d3d9e8395c3b5d44511212069fe92b94b89d7b57
Imran Shah <97672872+rmjdish@users.noreply.github.com>
2026-05-01 19:41:21 +0100

    gg

--- assets/js/browse_by_category.js
+++ assets/js/browse_by_category.js
@@ -41,6 +41,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const totalRows = data.length;
     const totalPages = Math.max(1, Math.ceil(totalRows / pageSize));
+	resultsCount.textContent = `Showing ${totalRows} results`;
     if (currentPage > totalPages) currentPage = totalPages;
 
     const start = (currentPage - 1) * pageSize;
