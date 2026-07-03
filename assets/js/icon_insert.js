d91b9e5279e8acb9630e5031f50e5756e19daa62
Imran Shah <97672872+rmjdish@users.noreply.github.com>
2026-07-03 20:08:23 +0100

    Update icon_insert.js

--- assets/js/icon_insert.js
+++ assets/js/icon_insert.js
@@ -35,6 +35,7 @@
 	"data coverage": 				"ti-chart-histogram",
 	"longitudinal variables":       "ti-repeat",
 	"longitudinal search":          "ti-repeat",
+	"the data dictionary":			"ti-book-2" 
     /* Section labels handled separately */
     /* Questionnaire waves */
     "explore nshd questionnaires":  "ti-clipboard-list",
