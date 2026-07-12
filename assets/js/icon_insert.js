0eeffb11d3dfbf2d1eb435a3555c9ba4ecab55bf
Imran Shah <97672872+rmjdish@users.noreply.github.com>
2026-07-12 19:14:16 +0100

    Update icon_insert.js

--- assets/js/icon_insert.js
+++ assets/js/icon_insert.js
@@ -40,6 +40,7 @@
 	"search by year":				"ti-calendar",
 	"search overview":				"ti-search",
 	"browse documentation":		"ti-versions",
+    "search documentation":		"ti-compass",
     /* Section labels handled separately */
     /* Questionnaire waves */
     "explore nshd questionnaires":  "ti-clipboard-list",
