5a839fbb2d55aa6f50846b636cccae97acf9227a
Imran Shah <imran.shah@ucl.ac.uk>
2026-08-12 10:43:41 +0100

    r

--- assets/js/popular_variables2.js
+++ assets/js/popular_variables2.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Load BOTH datasets
   Promise.all([
-    fetch("/OWL/docs/search_methods/popular_vars/popular_vars.json").then(r => r.json()),
+    fetch("/OWL/assets/data/popular_variables/popular_vars.json").then(r => r.json()),
     fetch("/OWL/docs/search_methods/data_dictionary/NSHD_Data_Dictionary_Public.json").then(r => r.json())
   ])
   .then(([popular, labels]) => {
